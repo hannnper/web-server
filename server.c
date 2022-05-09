643203136d5a83d13d3f5c28c313cf7cad5d726f
Hannah Perry <hperry1@student.unimelb.edu.au>
2022-05-09 18:09:19 +1000

    fix argv handling for protocol

--- server.c
+++ server.c
@@ -8,13 +8,19 @@
 
 
 #define _POSIX_C_SOURCE 200112L
+#include <netdb.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 int main(int argc, char** argv) {
+    int protocol;
+    char* port;
+    char* path;
 
     // print usage information if incorrect number command line arguments given
-    if (argc != 3) {
+    if (argc != 4) {
         fprintf(stderr, "./server usage:\n\t./server <protocol> <port> <path>"\
                         "\n\nprotocol: number, 4 (for IPv4) or 6 (for IPv6)\n"\
                         "port: port number\n"\
@@ -22,9 +28,24 @@ int main(int argc, char** argv) {
         exit(EXIT_FAILURE);
     }
 
-    int protocol = argv[1];
-    int port = argv[2];
-    char* path = argv[3];
+    // get protocol
+    if (strcmp(argv[1], "4") == 0) {
+        // using IPv4: AF_INET
+        protocol = AF_INET;
+    }
+    else if (strcmp(argv[1], "6") == 0) {
+        // using IPv6: AF_INET6
+        protocol = AF_INET6;
+    }
+    else {
+        // unrecognised protocol given
+        fprintf(stderr, "Error: unrecognised protocol (%s)", argv[1]);
+        exit(EXIT_FAILURE);
+    }
 
-    printf("%d %d %s",protocol, port, path);
+    // get port and path
+    port = argv[2];
+    path = argv[3];
+
+    printf("%d %s %s",protocol, port, path);
 }
